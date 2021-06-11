691b3f2f70169dd8539fb6e7c871fc72272fe012
Adnan Türk <adnan.turk@wirecardtr.onmicrosoft.com>
2021-06-11 09:21:38 +0300

    ....

--- Wirecard.Core/Dtos/UserAppDto.cs
+++ Wirecard.Core/Dtos/UserAppDto.cs
@@ -8,7 +8,7 @@ namespace Wirecard.Core.Dtos
 {
     public class UserAppDto
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string UserName { get; set; }
         public string EMail { get; set; }
         public string IdentityNumber { get; set; }
