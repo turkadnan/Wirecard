5eff4bac75a05b9baca86c3e3819ba68b0691533
Adnan Türk <adnan.turk@wirecardtr.onmicrosoft.com>
2021-06-21 07:25:51 +0300

    Token başka bir proje üzerinde test edildi

---
+++ TestApp1.API/Controllers/CountyController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace TestApp1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountyController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var userName = HttpContext.User.Identity.Name;
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            return Ok($"UserName:{userName}, UserId:{userIdClaim.Value}");
+        }
+
+    }
+}
